120d66b9f48613a2819ff1aa57fc55766f429777
allhoon18 <83628265+allhoon18@users.noreply.github.com>
2025-02-19 15:05:43 +0900

    [add] 미니게임의 최고기록을 보여주는 LeaderBoard 추가

---
+++ Assets/Scripts/LeaderBoardController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LeaderBoardController : MonoBehaviour
+{
+    public Button readButton;
+    public GameObject leaderBoardUI;
+    public Button leaderBoardCloseButton;
+    public TextMeshProUGUI flappyPlaneBestScore;
+
+    private void Awake()
+    {
+        readButton.onClick.AddListener(ShowLeaderBoard);
+        leaderBoardCloseButton.onClick.AddListener(CloseLeaderBoard);
+    }
+
+    private void ShowLeaderBoard()
+    {
+        leaderBoardUI.gameObject.SetActive(true);
+        UpdateFlappyPlaneBestScore();
+    }
+
+    private void UpdateFlappyPlaneBestScore()
+    {
+        flappyPlaneBestScore.text = PlayerPrefs.GetInt(GameManager.FlappyPlaneBestScoreKey).ToString();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        readButton.gameObject.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        readButton.gameObject.SetActive(false);
+        leaderBoardUI.gameObject.SetActive(false);
+    }
+
+    public void CloseLeaderBoard()
+    {
+        leaderBoardUI.gameObject.SetActive(false);
+    }
+}
