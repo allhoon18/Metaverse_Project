25efd573236d70adc9771229ec2c261f6966c071
allhoon18 <83628265+allhoon18@users.noreply.github.com>
2025-02-20 16:34:45 +0900

    [fix] 추가 주석 추가

--- Assets/Scripts/Object/LeaderBoardController.cs
+++ Assets/Scripts/Object/LeaderBoardController.cs
@@ -7,9 +7,13 @@ using UnityEngine.UI;
 
 public class LeaderBoardController : MonoBehaviour
 {
+    //리더보드 UI를 활성화하는 버튼 
     public Button readButton;
+    //리더보드 UI
     public GameObject leaderBoardUI;
+    //리더보드 UI를 비활성화하는 버튼
     public Button leaderBoardCloseButton;
+    //Flappy Plane 미니게임 최고기록 이름 및 점수
     public TextMeshProUGUI flappyPlaneBestScore;
     public TextMeshProUGUI flappyPlaneBestScoreName;
 
@@ -27,12 +31,14 @@ public class LeaderBoardController : MonoBehaviour
 
     private void UpdateFlappyPlaneBestScore()
     {
+        //PlayerPref에 저장된 이름과 점수를 불러옴
         flappyPlaneBestScore.text = PlayerPrefs.GetInt(GameManager.FlappyPlaneBestScoreKey).ToString();
         flappyPlaneBestScoreName.text = PlayerPrefs.GetString(GameManager.FlappyPlaneBestScoreNameKey);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //리더보드 오브젝트 주변 범위에 들어갔을 때 리더보드 활성화 버튼을 활성화
         readButton.gameObject.SetActive(true);
     }
 
